24173b1a7bdb4aef8ce350f9617388db7466da7c
Daniel Nevin Richards <dnevinrichards@berkeley.edu>
2025-01-04 00:59:42 -0800

    changed sqs queues to standard instead of FIFO because little documentation for FIFO

--- accumate_backend/celeryapp.py
+++ accumate_backend/celeryapp.py
@@ -2,6 +2,7 @@ import os
 from celery import Celery
 from .settings import SQS_LONG_RUNNING_URL, SQS_USER_INTERACTION_URL, SQS_DLQ_URL, SQS_CELERY_PIDBOX_URL
 import django
+from kombu import Queue
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accumate_backend.settings')
 
@@ -15,7 +16,7 @@ django.setup() # imports those installed apps
 app.autodiscover_tasks()
 
 # Explicitly disable dynamic reply queues, won't be able to use inspect command
-app.conf.worker_direct = False
+#app.conf.worker_direct = False
 
 app = Celery(
     "app",
@@ -23,32 +24,24 @@ app = Celery(
     broker_transport_options={
         "region": "us-west-1", # your AWS SQS region
         "predefined_queues": {
-            "ab-long-running-sqs.fifo": {  ## the name of the SQS queue
+            "ab-long-running": {  ## the name of the SQS queue
                 "url": SQS_LONG_RUNNING_URL
             },
-            "ab-user-interaction-sqs.fifo": {
+            "ab-user-interaction": {
                 "url": SQS_USER_INTERACTION_URL
             },
-            "ab-dlq-sqs.fifo": {
+            "ab-dlq": {
                 "url": SQS_DLQ_URL
             },
-            # "ab-celery-pidbox.fifo": {
-            #     "url": SQS_CELERY_PIDBOX_URL
-            # }
         },
     },
-    task_create_missing_queues=False,
+    task_create_missing_queues=True,
 )
 
 
 app.conf.broker_connection_retry_on_startup = True
-app.conf.task_default_queue = 'ab-long-running-sqs.fifo'
-
+app.conf.task_default_queue = 'ab-long-running'
 app.conf.result_backend = 'django-db'
-# app.conf.result_backend_transport_options = {
-#    'key_prefix': '{celery}'
-# }
-
 app.conf.accept_content = ["application/json"] 
 app.conf.task_serializer = "json"
 app.conf.result_serializer = "json" 
@@ -56,8 +49,3 @@ app.conf.broker_pool_limit = 1
 app.conf.broker_connection_timeout = 30
 app.conf.worker_prefetch_multiplier = 1
 #app.conf.redbeat_redis_url = REDIS_URL
-
-# app.conf.control_queue = 'ab-celery-pidbox.fifo'
-# app.conf.control_exchange = 'ab-celery-pidbox.fifo'
-# app.conf.control_exchange_type = 'fanout'
-# app.conf.control_routing_key = 'ab-celery-pidbox.fifo'
