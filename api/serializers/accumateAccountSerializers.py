97b290ee55d6ba39e23e148a934c32f3a8d01dee
Daniel Nevin Richards <dnevinrichards@berkeley.edu>
2025-01-19 14:22:55 -0800

    created website version of password reset flow

--- api/serializers/accumateAccountSerializers.py
+++ api/serializers/accumateAccountSerializers.py
@@ -22,10 +22,13 @@ class ResetPasswordRequestSerializer(serializers.Serializer):
 class ResetPasswordSerializer(serializers.Serializer):
     new_password = serializers.RegexField(
         regex=r'^(?=.*[A-Z])(?=.*\d)(?=.*[@$!\.%*?&])[A-Za-z\d@$!\.%*?&]{8,}$',
-        write_only=True,
-        error_messages={'invalid': ('Password must be at least 8 characters long with at least one capital letter and symbol')}
+        write_only=True
     )
-    confirm_password = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.RegexField(
+        regex=r'^(?=.*[A-Z])(?=.*\d)(?=.*[@$!\.%*?&])[A-Za-z\d@$!\.%*?&]{8,}$',
+        write_only=True,
+    )
+
 
 class WaitlistEmailSerializer(serializers.ModelSerializer):
     email = serializers.EmailField()
