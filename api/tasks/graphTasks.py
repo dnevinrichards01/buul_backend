8bb2ee6d73c6f219a02aa5b04704fef764da63af
Daniel Nevin Richards <dnevinrichards@berkeley.edu>
2025-04-03 22:46:37 -0700

    added graph files from plaid_integration which mistakenly were not merged before

---
+++ api/tasks/graphTasks.py
@@ -0,0 +1,248 @@
+from celery import shared_task, chord
+from django.core.cache import cache 
+from django.utils import timezone
+import json
+from ..models import User, StockData, Investments, UserInvestmentGraph
+
+from django.db.models import OuterRef, Subquery, JSONField, F, ExpressionWrapper, \
+    DurationField, Window
+from django.db.models.functions import Lead
+from django.contrib.postgres.aggregates import JSONBAgg
+from api.yahooRapidApiClient import fpm_client, FPMClient, FPMUtils
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from zoneinfo import ZoneInfo
+
+from django.apps import apps
+
+
+@shared_task(name="refresh_stock_data_by_interval")
+def refresh_stock_data_by_interval(symbols=["VOO", "VOOG", "QQQ", "IBIT"], 
+                       interval="1d", refresh_all=False):
+    # import pdb
+    # breakpoint()
+
+    curr_minute_rounded = FPMUtils.round_date_down(
+            timezone.now(), 
+            granularity="1m"
+        )
+
+    # decide whether to refresh, and when to refresh from
+    most_recent_refresh_date_query = StockData.objects.all()
+    if not refresh_all and most_recent_refresh_date_query.exists():
+        most_recent_refresh_date = most_recent_refresh_date_query\
+            .order_by("-date").first().date
+        most_recent_refresh_date_rounded = FPMUtils.round_date_down(
+            most_recent_refresh_date.replace(tzinfo=ZoneInfo("UTC")), 
+            granularity=interval
+        )
+        curr_date_rounded = FPMUtils.round_date_down(
+            timezone.now(), 
+            granularity=interval
+        )
+        # if current date is more recent than last refresh, then must refresh
+        if curr_date_rounded > most_recent_refresh_date_rounded:
+            oldest_queryable_data = curr_date_rounded - FPMUtils.get_maximum_range(interval)
+            # refresh from most recent refresh date. 
+            # if that is too far in the past, refresh from oldest possible date
+            start_date_rounded = max(
+                oldest_queryable_data, 
+                most_recent_refresh_date_rounded
+            )
+        else:
+            return 
+    else:
+        # if this is our first time
+        start_date_rounded = FPMUtils.round_date_down(
+            timezone.now() - FPMUtils.get_maximum_range(interval), 
+            granularity=interval
+        )
+
+    # for each security...
+    for symbol in symbols:
+        # fetch data...
+        response = fpm_client.get_historical(symbol, start_date_rounded, 
+                                            timezone.now(), interval)
+        # and save it to db
+        for item in response:
+            item_date = FPMUtils.no_timezone_to_with_timezone(item["date"], interval)
+            if item_date < start_date_rounded:
+                continue
+            item_price = item["close"]
+            stockData, created = StockData.objects.get_or_create(
+                date = item_date
+            )
+            if stockData[symbol]:
+                continue
+            else:
+                stockData[symbol] = item_price 
+                stockData.save()
+
+    # for symbol in symbols:
+
+        null_prices = StockData.objects.filter(**{
+            f"{symbol}__isnull": True,
+            "date__gte": start_date_rounded
+        })
+        if null_prices.exists():
+            most_recent_prices = null_prices.annotate(
+                prev_price = Subquery((
+                    StockData.objects.filter(**{
+                        f"{symbol}__isnull": False,
+                        "date__lte": OuterRef("date")
+                    })
+                    .order_by("-date")
+                    .values(symbol)[:1]
+                ))
+            )
+            for item in most_recent_prices:
+                item[symbol] = item.prev_price
+            StockData.objects.bulk_update(most_recent_prices, [symbol])
+
+
+        null_prices = StockData.objects.filter(**{
+            f"{symbol}__isnull": True,
+            "date__gte": start_date_rounded
+        })
+        if null_prices.exists():
+            prices_immediately_after = null_prices.annotate(
+                price_immediately_after = Subquery((
+                    StockData.objects.filter(**{
+                        f"{symbol}__isnull": False,
+                        "date__gte": OuterRef("date")
+                    })
+                    .order_by("date")
+                    .values(symbol)[:1]
+                ))
+            )
+            for item in prices_immediately_after:
+                item[symbol] = item.price_immediately_after
+            StockData.objects.bulk_update(prices_immediately_after, [symbol])
+
+        # if interval == "1m":
+        #     time_gap_query = StockData.objects.filter(
+        #         date__gte=curr_minute_rounded-relativedelta(hours=24))\
+        #         .annotate(**{
+        #             "next_date": Window(
+        #                 expression=Lead('date'),
+        #                 order_by=F('date').asc()
+        #             ),
+        #             "time_gap": ExpressionWrapper(
+        #                 F('next_date') - F('date'),
+        #                 output_field=DurationField()
+        #             )
+        #         }) \
+        #         .filter(time_gap__gt=relativedelta(minutes=1))
+        #     for item in time_gap_query:
+        #         created = True
+        #         item_date = item.date + relativedelta(minutes=1)
+        #         while created and item_date < item.next_date:
+        #             stockData, created = StockData.objects.get_or_create(
+        #                 date = item_date,
+        #                 defaults={symbol: item[symbol]}
+        #             )
+        #             item_date += relativedelta(minutes=1)
+
+    return "done"
+
+def refresh_stock_data_all():
+    for interval in ["1m", "1h", "1d"]:
+        refresh_stock_data_by_interval(interval=interval, refresh_all=True)
+    delete_non_closing_times()
+
+# on a timer?
+@shared_task(name="delete_non_closing_times")
+def delete_non_closing_times():
+    for interval in ["1m", "1h", "1d", "1w", "1M"]:
+        current_date_rounded = FPMUtils.round_date_down(
+            timezone.now(), 
+            granularity="1m"
+        )
+        FPMUtils.delete_non_closing_times(current_date_rounded, interval)
+
+@shared_task(name="get_graph_data")
+def get_graph_data(uid, symbols=["VOO", "VOOG", "QQQ", "IBIT"]):
+    try:
+        # import pdb 
+        # breakpoint()
+
+        user = User.objects.get(id=uid)
+
+
+        # the last time they requested graph data
+        last_saved_date_query = UserInvestmentGraph.objects.filter(user=user)\
+            .order_by('-date')
+        if last_saved_date_query.exists():
+            last_saved_date = last_saved_date_query.first()
+            start_date = FPMUtils.round_date_down(
+                last_saved_date.date, 
+                granularity="1min"
+            )
+        # if they've never requested graph data, start from beginning
+        else:
+            # their first investment
+            first_investment_date_query = Investments.objects.filter(user=user)\
+                .order_by("date")
+            if first_investment_date_query.exists():
+                first_investment_date = first_investment_date_query.first().date
+            else:
+                first_investment_date = None
+
+            # date five years ago
+            five_years_ago = FPMUtils.round_date_down(
+                timezone.now() - relativedelta(years=5), 
+                granularity="1min"
+            )
+
+            # start_date = whichever is oldest
+            if first_investment_date is None:
+                start_date = five_years_ago
+            else:
+                start_date = min(first_investment_date, five_years_ago)
+        
+        cumulative_quantity_subquery = (
+            Investments.objects.filter(
+                user=user,
+                date__lte=OuterRef("date")
+            )
+            .order_by("-date")
+            .values("cumulative_quantities")[:1]
+        )
+        # Annotate Investments with the JSON-aggregated cumulative quantities
+        queryset = StockData.objects \
+            .filter(date__gte=start_date) \
+            .annotate(
+                cumulative_quantities=Subquery(cumulative_quantity_subquery)
+            )
+        
+        # breakpoint()
+        
+        for stockData in queryset:
+            quantities = stockData.cumulative_quantities or {}
+            price = 0
+            for symbol in quantities:
+                price += stockData[symbol] * (quantities[symbol] or 0)
+            userInvestmentGraph, created = UserInvestmentGraph.objects.get_or_create(
+                user = user,
+                date = stockData.date,
+                defaults = {'value': price}
+            )
+            if created:
+                userInvestmentGraph.save()
+        
+        # breakpoint()
+        
+        cache.delete(f"uid_{uid}_get_investment_graph_data")
+        cache.set(
+            f"uid_{uid}_get_investment_graph_data",
+            json.dumps({"success": "calculated and saved", "error": None}),
+            timeout=120
+        )
+    except ImportError as e:
+        cache.delete(f"uid_{uid}_get_investment_graph_data")
+        cache.set(
+            f"uid_{uid}_get_investment_graph_data",
+            json.dumps({"success": None, "error": f"error {str(e)}"}),
+            timeout=120
+        )
+            
---
+++ api/yahooRapidApiClient.py
@@ -0,0 +1,208 @@
+import requests
+from accumate_backend.settings import FMP_KEY
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
+from zoneinfo import ZoneInfo
+from api.models import StockData, UserInvestmentGraph
+import json
+from django.db.models import OuterRef, Subquery, Max, F
+
+class FPMUtils:
+    interval_char_to_str = {
+        "m": "Minute",
+        "h": "Hour",
+        "d": "Day",
+        "w": "Day",
+        "M": "Month"
+    }
+    # i think we'll have a main order table which links to other ones with an id/etc?
+    brokerage_to_order_model = {
+        "robinhood": "RobinhoodStockOrder"
+    }
+
+    @classmethod
+    def round_date_down(cls, date, granularity="1d"):
+        n = int(granularity[0])
+        unit = granularity[1]
+        
+        if unit == "m":
+            delta = relativedelta(minutes=n-1)
+            return date.replace(second=0, microsecond=0) - delta
+        elif unit == "h":
+            delta = relativedelta(hours=n-1)
+            return date.replace(minute=0, second=0, microsecond=0) - delta
+        elif unit == "d":
+            delta = relativedelta(days=n-1)
+            return date.replace(hour=0, minute=0, second=0, microsecond=0) - delta
+        elif unit == "w":
+            days_since_start_of_week = relativedelta(days=date.weekday())
+            delta = relativedelta(weeks=n-1)
+            return date.replace(
+                hour=0, 
+                minute=0, 
+                second=0, 
+                microsecond=0
+            ) - days_since_start_of_week - delta
+        elif unit == "M":
+            delta = relativedelta(months=n-1)
+            return date.replace(
+                day=1,
+                hour=0, 
+                minute=0, 
+                second=0, 
+                microsecond=0
+            ) - delta
+
+    @classmethod
+    def get_time_delta(cls, delta_str):
+        n = delta_str[0]
+        unit = delta_str[1]
+        
+        if unit == "m":
+            return relativedelta(minutes=n)
+        elif unit == "h":
+            return relativedelta(hours=n)
+        elif unit == "d":
+            return relativedelta(days=n)
+        elif unit == "w":
+            return relativedelta(weeks=n)
+        elif unit == "M":
+            return relativedelta(month=n)
+    
+    @classmethod
+    def get_maximum_range(cls, delta_str):
+        n = delta_str[0]
+        unit = delta_str[1]
+        
+        if unit == "m":
+            return relativedelta(days=1)
+        elif unit == "h":
+            return relativedelta(months=3)
+        elif unit == "d":
+            return relativedelta(years=1)
+        elif unit == "w":
+            return relativedelta(years=5)
+        elif unit == "M":
+            return relativedelta(years=100)
+        else:
+            raise ValueError("unit must be from ['m', 'h', 'd', 'w', 'M']")
+    
+    @classmethod
+    def no_timezone_to_with_timezone(cls, date_str, interval):
+        if interval[1] in ["m", "h"]:
+            naive_dt = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S") 
+        else:
+            naive_dt = datetime.strptime(date_str, "%Y-%m-%d")
+        eastern_dt = naive_dt.replace(tzinfo=ZoneInfo("America/New_York"))
+        utc_dt = eastern_dt.astimezone(ZoneInfo("UTC"))
+        return utc_dt
+    
+    @classmethod
+    def delete_non_closing_times(cls, current_date_rounded, interval):
+        if interval[1] == "m":
+            return
+        if interval[1] == "h":
+            delta = relativedelta(months=3)
+            kwargs = {
+                "date__hour": OuterRef("date__hour"),
+                "date__date": OuterRef("date__date")
+            }
+        elif interval[1] == "d":
+            delta = relativedelta(years=1)
+            kwargs = {
+                "date__date": OuterRef("date__date")
+            }
+        elif interval[1] == "w":
+            delta = relativedelta(years=1)
+            kwargs = {
+                "date__week": OuterRef("date__week"),
+                "date__year": OuterRef("date__year")
+            }
+        elif interval[1] == "M":
+            delta = relativedelta(years=5)
+            kwargs = {
+                "date__month": OuterRef("date__month"),
+                "date__year": OuterRef("date__year")
+            }
+
+        latest_date_subquery = StockData.objects.filter(**kwargs) \
+            .order_by("-date").values("date")[:1]
+        StockData.objects \
+            .filter(date__lte = current_date_rounded - delta) \
+            .exclude(date=Subquery(latest_date_subquery)) \
+            .delete()
+        
+        latest_date_subquery = UserInvestmentGraph.objects.filter(**kwargs) \
+            .order_by("-date").values("date")[:1]
+        UserInvestmentGraph.objects \
+            .filter(date__lte = current_date_rounded - delta) \
+            .exclude(date=Subquery(latest_date_subquery)) \
+            .delete()
+        return
+       
+
+class FPMClient:
+    root_url = "https://financialmodelingprep.com/stable/"
+    possible_intervals = ["1min", "5min", "15min", "30min", "1hour", "4hour"]
+    
+
+    def __init__(self, key):
+        self.key = key
+
+    def get_historical(self, symbol, start, end, interval):
+        
+        if interval[1] in ["m", "h"]:
+            return self.get_intra_day(symbol, start, end, interval)
+        elif interval[1] in ["d", "M"]:
+            return self.get_eod(symbol, start, end)
+        else: 
+           interval_formatted = interval
+        if not interval_formatted in self.possible_intervals:
+            raise ValueError(f"interval must be in {self.possible_intervals}")
+    
+    def get_intra_day(self, symbol, start, end, interval):
+        if interval[1] == "m":
+            interval_formatted = f"{interval[0]}min"
+        elif interval[1] == "h":
+            interval_formatted = f"{interval[0]}hour"
+        else: 
+            interval_formatted = interval
+        if not interval_formatted in self.possible_intervals:
+            raise ValueError(f"interval must be in {["m", "h"]}")
+        
+        response = requests.get(
+            self.root_url + f"historical-chart/{interval_formatted}",
+            params={
+                "from": start.strftime("%Y-%m-%d"),
+                "to": end.strftime("%Y-%m-%d"),
+                "apikey": self.key,
+                "nonadjusted": False,
+                "symbol": symbol
+            }
+        )
+        if response.status_code < 200 or response.status_code > 299:
+            raise ConnectionError(f"fpm response has status code {response.status_code}: {response.content}")
+        return json.loads(response.content.decode("utf-8"))
+
+    def get_eod(self, symbol, start, end):
+        response = requests.get(
+            self.root_url + f"historical-price-eod/full",
+            params={
+                "from": start.strftime("%Y-%m-%d"),
+                "to": end.strftime("%Y-%m-%d"),
+                "apikey": self.key,
+                "symbol": symbol
+            }
+        )
+        if response.status_code < 200 or response.status_code > 299:
+            raise ConnectionError(f"fpm response has status code {response.status_code}: {response.content}")
+        return json.loads(response.content.decode("utf-8"))
+
+   
+
+
+
+
+
+fpm_client = FPMClient(FMP_KEY)
